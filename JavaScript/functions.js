060c36dce1662afa72d7578efa4ded91c8e64e2c
Ajay <asa.spikman@student.alfa-college.nl>
2026-04-01 12:01:42 +0200

    html versie 1

---
+++ JavaScript/functions.js
@@ -0,0 +1,5 @@
+function toonScherm2() {
+    document.getElementById('Begin scherm').style.display = 'none';
+    document.getElementById('Spel').style.display = 'block';
+    document.getElementById('Eind scherm').style.display= 'block';
+}
