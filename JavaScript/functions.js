e2fdc56c0d63201fadf173e312d664383c43e09b
Ajay <asa.spikman@student.alfa-college.nl>
2026-04-07 13:38:09 +0200

    buttons werken

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -1,5 +1,27 @@
-function toonScherm2() {
-    document.getElementById('Begin scherm').style.display = 'none';
-    document.getElementById('Spel').style.display = 'block';
-    document.getElementById('Eind scherm').style.display= 'block';
+function showBegin() {
+    document.querySelector(".begin").style.display = "flex";
+    document.querySelector(".spel").style.display = "none";
+    document.querySelector(".informatie").style.display = "none";
+    document.querySelector(".eind").style.display = "none";
+}
+
+function showSpel() {
+    document.querySelector(".begin").style.display = "none";
+    document.querySelector(".spel").style.display = "flex";
+    document.querySelector(".informatie").style.display = "none";
+    document.querySelector(".eind").style.display = "none";
+}
+
+function showInformatie() {
+    document.querySelector(".begin").style.display = "none";
+    document.querySelector(".spel").style.display = "none";
+    document.querySelector(".informatie").style.display = "flex";
+    document.querySelector(".eind").style.display = "none";
+}
+
+function showEind() {
+    document.querySelector(".begin").style.display = "none";
+    document.querySelector(".spel").style.display = "none";
+    document.querySelector(".informatie").style.display = "none";
+    document.querySelector(".eind").style.display = "flex";
 }
