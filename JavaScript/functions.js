524cf79d21e031a9f8b49234ad75bbe2be6c1713
Ajay <asa.spikman@student.alfa-college.nl>
2026-04-08 11:39:39 +0200

    vragen en JS netter

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -1,27 +1,114 @@
+let vragen = [
+    {
+        tekst: "Is dit boek van ....?",
+        antwoorden: ["jou", "jouw"],
+        correct: 0
+    },
+    {
+        tekst: "Ik heb .... gisteren nog gebeld.",
+        antwoorden: ["jou", "jouw"],
+        correct: 1
+    },
+    {
+        tekst: "Waar ligt .... tas?",
+        antwoorden: ["jou", "jouw"],
+        correct: 0
+    },
+    {
+        tekst: "Ik ben benieuwd naar .... mening.",
+        antwoorden: ["jou", "jouw"],
+        correct: 0
+    },
+    {
+        tekst: "Ik wil graag met .... praten.",
+        antwoorden: ["jou", "jouw"],
+        correct: 0
+    },
+    {
+        tekst: "Dit is .... fiets, toch?",
+        antwoorden: ["jou", "jouw"],
+        correct: 1
+    },
+    {
+        tekst: "Hij geeft .... een cadeau.",
+        antwoorden: ["jou", "jouw"],
+        correct: 0
+    },
+    {
+        tekst: "Is dat .... idee geweest?",
+        antwoorden: ["jou", "jouw"],
+        correct: 0
+    },
+    {
+        tekst: "Ik zie .... daar staan.",
+        antwoorden: ["jou", "jouw"],
+        correct: 0
+    },
+    {
+        tekst: "Hoe gaat het met .... ouders?",
+        antwoorden: ["jou", "jouw"],
+        correct: 1
+    },
+    {
+        tekst: "Ik vertrouw .... volledig.",
+        antwoorden: ["jou", "jouw"],
+        correct: 0
+    },
+    {
+        tekst: "Heb je .... telefoon al gevonden?",
+        antwoorden: ["jou", "jouw"],
+        correct: 0
+    },
+    {
+        tekst: "Ze helpt .... met huiswerk.",
+        antwoorden: ["jou", "jouw"],
+        correct: 0
+    },
+    {
+        tekst: "Wat vind je van .... nieuwe baan?",
+        antwoorden: ["jou", "jouw"],
+        correct: 1
+    },
+    {
+        tekst: "Ik kan niet zonder .... .",
+        antwoorden: ["jou", "jouw"],
+        correct: 0
+    },
+    {
+        tekst: "`Is dit .... jas of die van iemand anders?",
+        antwoorden: ["jou", "jouw"],
+        correct: 0
+    }
+
+];
+
+function showElement(begin, spel, informatie, eind) 
+{
+    document.querySelector(".begin").style.display = begin;
+    document.querySelector(".spel").style.display = spel;
+    document.querySelector(".informatie").style.display = informatie;
+    document.querySelector(".eind").style.display = eind;
+}
+
 function showBegin() {
-    document.querySelector(".begin").style.display = "flex";
-    document.querySelector(".spel").style.display = "none";
-    document.querySelector(".informatie").style.display = "none";
-    document.querySelector(".eind").style.display = "none";
+    showElement("flex", "none", "none", "none");
 }
 
 function showSpel() {
-    document.querySelector(".begin").style.display = "none";
-    document.querySelector(".spel").style.display = "flex";
-    document.querySelector(".informatie").style.display = "none";
-    document.querySelector(".eind").style.display = "none";
+    showElement("none", "flex", "none", "none");
+    let tekstElement = document.getElementById("tekst");
+    let antwoordKnoppen = document.querySelectorAll(".Game_Button");
+
+    antwoordKnoppen.forEach((knop, index) => {
+        knop.textContent = vragen[1].antwoorden[index];
+    });
+    tekstElement.textContent = vragen[1].tekst;
 }
 
 function showInformatie() {
-    document.querySelector(".begin").style.display = "none";
-    document.querySelector(".spel").style.display = "none";
-    document.querySelector(".informatie").style.display = "flex";
-    document.querySelector(".eind").style.display = "none";
+    showElement("none", "none", "flex", "none");
 }
 
 function showEind() {
-    document.querySelector(".begin").style.display = "none";
-    document.querySelector(".spel").style.display = "none";
-    document.querySelector(".informatie").style.display = "none";
-    document.querySelector(".eind").style.display = "flex";
+    showElement("none", "none", "none", "flex");
 }
